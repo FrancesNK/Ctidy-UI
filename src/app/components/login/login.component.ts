3c58ecb7dd8c4b591e9149109338070ecba418c4
Frances Shi <shiyufeng1990@foxmail.com>
2017-11-26 08:29:46 -0500

    add login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent {
-  let i : string = "yufengs";
+
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -5,8 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { LoginComponent } from '../../components/login/login.component';
-
 
 @NgModule({
   declarations: [
