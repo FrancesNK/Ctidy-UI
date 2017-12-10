ff1fc402ad5989fd7c3dac5b9f533b400071684c
Frances Shi <shiyufeng1990@foxmail.com>
2017-12-10 06:49:19 -0500

    1. fix the issue of cannot have style for the element 2. complete the basic user page

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
     public username: string;
     public followed: number;
+    public moments: number;
+    public following: number;
     public usercity: Array<string> = [];
     public ngOnInit() {
         this.getUsername();
@@ -19,14 +21,17 @@ export class UserComponent implements OnInit {
         this.username = 'FrancesNK';
         this.usercity = ['江苏省', '南京市'];
         this.followed = 2;
+        this.following = 10;
+        this.moments = 119;
     }
 
     public getBackground() {
         const element = document.getElementsByClassName('user-top');
+        const element1 = document.getElementById('user-top');
         const profilePicUrl = 'http://static1.keepcdn.com/avatar/2017/11/29/22/79bf33e80c987ffa19cd99b50eb2f8d4d2ca60d5.jpg';
         const style = `background-image: url(${profilePicUrl})`;
         if (element.length) {
-            element[0].style = style;
+            element[0].setAttribute('style', style);
         }
     }
 
@@ -36,7 +41,7 @@ export class UserComponent implements OnInit {
         'http://static1.keepcdn.com/avatar/2017/01/05/12/1beae65b5f1eb2814852ec75a5306d7c5c361781.jpg?imageMogr2/thumbnail/96x';
         const style = `background-image: url(${profilePicUrl})`;
         if (element.length) {
-            element[0].style = style;
+            element[0].setAttribute('style', style);
         }
     }
 }
