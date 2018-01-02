b762d4b5410cdd8469513ddc3716f3e5ea857298
Frances Shi <shiyufeng1990@foxmail.com>
2018-01-02 07:59:59 -0500

    fix service

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -22,25 +22,26 @@ export class UserComponent implements OnInit {
         private userInfoService: UserInfoService
     ) {}
 
-    public ngOnInit() {
-        this.getUserInfo();
+    public async ngOnInit() {
+        await this.getUserConfig();
         this.getUsername();
         this.setBackground();
         this.setAvatar();
     }
 
-    private getUserInfo(): void {
-        this.userInfoPromise = Promise.all([
+    private async getUserConfig(): Promise <any> {
+        await Promise.all([
              this.userInfoService.getUserInfo()
         ]).then(async (results: any[]) => {
             if (!Array.isArray(results) || results.length !== 1) {
                 return;
             }
 
-            this.userInfo = new UserInfoModel ({});
-            this.userInfo.userId = results[0].userId;
-            this.userInfo = new UserInfoModel (results[0]);
-
+            if (Array.isArray(results[0])) {
+                results[0].forEach(userInfo => {
+                    this.userInfo = new UserInfoModel (userInfo);
+                });
+            }
         });
     }
 
@@ -55,9 +56,7 @@ export class UserComponent implements OnInit {
     public setBackground() {
         const element = document.getElementsByClassName('user-top');
         const element1 = document.getElementById('user-top');
-        const profilePicUrl =
-            this.userInfo.background ? this.userInfo.background :
-                'http://static1.keepcdn.com/avatar/2017/11/29/22/79bf33e80c987ffa19cd99b50eb2f8d4d2ca60d5.jpg';
+        const profilePicUrl = this.userInfo.background;
         const style = `background-image: url(${profilePicUrl})`;
         if (element.length) {
             element[0].setAttribute('style', style);
@@ -66,9 +65,7 @@ export class UserComponent implements OnInit {
 
     public setAvatar() {
         const element = document.getElementsByClassName('user-avatar');
-        const profilePicUrl =
-            this.userInfo.avatar ? this.userInfo.avatar :
-                'http://static1.keepcdn.com/avatar/2017/01/05/12/1beae65b5f1eb2814852ec75a5306d7c5c361781.jpg?imageMogr2/thumbnail/96x';
+        const profilePicUrl = this.userInfo.avatar;
         const style = `background-image: url(${profilePicUrl})`;
         if (element.length) {
             element[0].setAttribute('style', style);
--- src/app/services/user-info/user-info.service.ts
+++ src/app/services/user-info/user-info.service.ts
@@ -9,7 +9,7 @@ export class UserInfoService {
             email: 'frances@ctidy.com',
             phoneNumber: '11122223333',
             address: '江苏 南京',
-            avato: 'http://static1.keepcdn.com/avatar/2017/01/05/12/1beae65b5f1eb2814852ec75a5306d7c5c361781.jpg?imageMogr2/thumbnail/96x',
+            avatar: 'http://static1.keepcdn.com/avatar/2017/01/05/12/1beae65b5f1eb2814852ec75a5306d7c5c361781.jpg?imageMogr2/thumbnail/96x',
             background: 'http://static1.keepcdn.com/avatar/2017/11/29/22/79bf33e80c987ffa19cd99b50eb2f8d4d2ca60d5.jpg'
         }]);
     }
